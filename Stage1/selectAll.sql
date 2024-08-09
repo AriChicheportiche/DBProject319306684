3e0fef3821254d97961e32cbde13806aa71fc3fb
Aribo <63457208+AriChicheportiche@users.noreply.github.com>
2024-08-09 15:39:00 +0300

    Update selectAll.sql

--- Stage1/selectAll.sql
+++ Stage1/selectAll.sql
@@ -1,1 +1,9 @@
+-- Select all data from Borrower table
+SELECT * FROM Borrower;
+
+-- Select all data from Loan table
+SELECT * FROM Loan;
+
+-- Select all data from PaymentSchedule table
+SELECT * FROM PaymentSchedule;
 
